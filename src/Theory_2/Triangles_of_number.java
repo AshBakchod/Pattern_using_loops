42a9cb79315d6c18d83ec2ff609764928f19ba16
Harshal <harshal19052003@gmail.com>
2023-03-27 13:47:02 +0530

    comit

--- src/Theory_2/Triangles_of_number.java
+++ src/Theory_2/Triangles_of_number.java
@@ -4,35 +4,33 @@ public class Triangles_of_number {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
-        int n =1;
-        int i = 1;
+        int n = 1;
 
-        if (N>=0 && N<=50){
+        if (N<=50 && N>=0){
+            while (n<=N){
+               int i=1;
 
-            while(n<=N){
+               while(i<=N+n-1){
 
-                i=1;
-
-                while(i<=(N+n-1)){
-
-                    int p = 0;
-
-                    if (i<=(N-n)){
-                        System.out.print(" ");
-                    }else if(i>(N-n) && i<N){
-                        System.out.print(n+p);
-                    }else if (i==N){
-                        System.out.print(2*n-1);
-                    }
-                     else if (i>N && i<(N-n+1)){
-                         System.out.print(n-p);}
-                     else if (i==(N-n+1)){
-                         System.out.println(n);
-                    }
-                    i++;
-                     p++;
-                }
-             n++;
+                   if(i<=N-n){
+                       System.out.print(" ");
+                   } else if (i>(N-n) && i<=(N)) {
+                       int p = n;
+                       while(p<=2*n-1){
+                        System.out.print(p);
+                        p++;
+                       }
+                   }else if (i>N && i<=(N+n-1)){
+                       int j = 2*n-2;
+                       while(j>=n){
+                           System.out.print(j);
+                           j--;
+                       }
+                   }
+                   i++;
+               }
+               System.out.println("");
+               n++;
             }
         }
     }
